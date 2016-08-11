cb8032757fa1bf7cf733abd007a42fecee9defc2
marcoaureliodiasamerico@gmail.com <marcoaureliodiasamerico@gmail.com>
2016-08-11 19:28:02 -0300

    Começou a bagaça

--- CarSocialNetwork/ViewController.swift
+++ CarSocialNetwork/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Firs")
     }
 
     override func didReceiveMemoryWarning() {
