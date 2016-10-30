28cf453cdb5e5656d9957f5f769b122e74a91407
Marco Aurelio <marcoaurelio@marcos-mbp>
2016-10-29 22:16:18 -0200

    10.1 na veia...

--- CarSocialNetwork/AppDelegate.swift
+++ CarSocialNetwork/AppDelegate.swift
@@ -18,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        UIApplication.shared.setStatusBarStyle(UIStatusBarStyle.lightContent, animated: true)
-        
         configureParse()
         AppCongifuration.systemBars()
         setIncialStoryBoard()
